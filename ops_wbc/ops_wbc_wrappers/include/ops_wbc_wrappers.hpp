c40e45417e1393c498b14e2782bdcc2894a48810
ZhengYi0310 <hczhengcq@gmail.com>
2018-01-24 13:50:49 -0800

    xhand.cpp

---
+++ ops_wbc/ops_wbc_wrappers/include/ops_wbc_wrappers.hpp
@@ -0,0 +1,17 @@
+#ifndef __OPS_WBC_WRAPPERS_OPS_WBC_WRAPPERS_HPP
+#define __OPS_WBC_WRAPPERS_OPS_WBC_WRAPPERS_HPP
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+//#include <ops_wbc_wrappers/render.hpp>
+#include <ops_wbc_wrappers/material.hpp>
+#include <ops_wbc_wrappers/exceptions.hpp>
+#include <ops_wbc_wrappers/x_object.hpp>
+#include <ops_wbc_wrappers/x_deformable_object.hpp>
+#include <ops_wbc_wrappers/simple_object.hpp>
+#include <ops_wbc_wrappers/grid.hpp>
+#include <ops_wbc_wrappers/coordinate.hpp>
+#include <ops_wbc_wrappers/pipe.hpp>
+
+#endif /* __OPS_WBC_WRAPPERS_OPS_WBC_WRAPPERS_HPP */
