0eaf872e58e9c45f084efd96588e0416c854fa6a
ZhengYi0310 <hczhengcq@gmail.com>
2018-02-05 17:19:04 -0800

    all tests

---
+++ ops_wbc/ops_wbc_robot/test/test5.cpp
@@ -0,0 +1,84 @@
+/*************************************************************************
+	> File Name: test5.cpp
+	> Author: Yi Zheng 
+	> Mail: hczhengcq@gmail.com
+	> Created Time: Mon 05 Feb 2018 05:09:31 PM PST
+ ************************************************************************/
+
+#include <ros/ros.h>
+#include <utils/exception.hpp>
+#include "ops_wbc_robot/robot/parser.hpp"
+#include "ops_wbc_robot/robot/tf_publisher.hpp"
+using namespace ops_wbc_robot;
+
+Eigen::MatrixXd M;
+Eigen::MatrixXd J;
+Eigen::MatrixXd T0, T1, T2;
+
+void calc()
+{
+  //std::cout << "M : " << std::endl << M.inverse() << std::endl;
+  //          << "J : " << std::endl << J << std::endl << std::endl;
+  Eigen::MatrixXd M_inv = M.inverse();
+  Eigen::MatrixXd Jv = J.block(0, 0, 3, J.cols());
+  Eigen::MatrixXd Lambda_inv = Jv * M_inv * Jv.transpose();
+  Eigen::MatrixXd Lambda = Lambda_inv.inverse();
+  Eigen::MatrixXd J_dyn_inv = M_inv * Jv.transpose() * Lambda;
+  Eigen::MatrixXd I = Eigen::MatrixXd::Identity(15, 15);
+  Eigen::MatrixXd N = I - J_dyn_inv * Jv;
+
+  //std::cout << N.transpose() << std::endl << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "parser_test");
+    ros::NodeHandle nh;
+
+    try
+    {
+        std::string name = "red_arm2";
+        RobotPtr robot = std::make_shared<Robot>(name);
+
+        ParserPtr parser = std::make_shared<Parser>();
+        std::string path = "/home/daichi/Work/catkin_ws/src/ahl_ros_pkg/ahl_robot/ahl_robot/yaml/red_arm2.yaml";
+        parser->load(path, robot);
+
+        ros::MultiThreadedSpinner spinner;
+
+        TfPublisherPtr tf_publisher = std::make_shared<TfPublisher>();
+
+        const std::string mnp_name = "mnp";
+        unsigned long cnt = 0;
+        ros::Rate r(10.0);
+
+        while(ros::ok())
+        {
+            Eigen::VectorXd q = Eigen::VectorXd::Zero(robot->getDOF(mnp_name));
+            ManipulatorPtr mnp = robot->getManipulator(mnp_name);
+
+            double goal = sin(2.0 * M_PI * 0.2 * cnt * 0.1);
+            ++cnt;
+
+            std::cout << M_PI / 4.0 * goal << std::endl;
+            q = Eigen::VectorXd::Constant(q.rows(), 0.0 * M_PI / 4.0);
+
+            robot->update(q);
+            robot->computeJacobian(mnp_name);
+            robot->computeMassMatrix(mnp_name);
+            //std::cout << robot->getMassMatrix(mnp_name) << std::endl << std::endl;
+            //std::cout << robot->getJacobian(mnp_name, "gripper") << std::endl << std::endl;
+
+            //calc();
+            tf_publisher->publish(robot, false);
+            r.sleep();
+        }
+    }
+    catch(ops_wbc_utils::Exception& e)
+    {
+        ROS_ERROR_STREAM(e.what());
+    }
+
+    return 0;
+}
+
